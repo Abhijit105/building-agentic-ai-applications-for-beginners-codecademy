bf81970591840df3abc5e44c2370dba37af3a5b9
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2025-11-21 23:28:19 +0530

    feat(numpy): day 04 revision

--- numpy-workspace/lecture-04/prog-01.py
+++ numpy-workspace/lecture-04/prog-01.py
@@ -1,17 +1,30 @@
 import numpy as np
-import pandas as pd
 
 a = np.array([[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14]])
 print(a)
+print(type(a))
+print(a.shape)
+print(len(a))
+
+b = np.array([1.3, 2, 3, 'Mathematics', 50])
+print(b)
+print(type(b))
+print(len(b))
+print(b[3])
+print(b[3][-6:])
 
 a[0][4] = 50
 print(a)
 a[0, 4] = 500
 print(a)
+print(a[0, 4])
 
 print(np.zeros((3, 3)))
 print(np.zeros((3, 3), dtype='int'))
 print(np.ones((3, 3), dtype='int'))
 
+print(np.full((3, 3), 15, dtype='float'))
+
 print(np.random.rand(4, 4))
 print(np.random.randint(1, 10, size=(3, 3)))
+print(np.random.uniform(1, 10, size=(3, 3)))
--- numpy-workspace/lecture-04/prog-02.py
+++ numpy-workspace/lecture-04/prog-02.py
@@ -1,2 +1,32 @@
-# Given a positive odd number n(> 2), create a numpy array of size nxn with all zeros and ones such that the ones make a shape of a plus 
+# Given a positive odd number n(> 2), create a numpy array of size nxn with all zeros and ones such that the ones make a shape of a plus(+). If n is even, then the function should return "Please enter an odd number greater than 2"
 
+import numpy as np
+
+def make_plus(n):
+    if n <= 2 or n % 2 == 0:
+        print("Please enter an odd number greater than 2")
+        return
+
+    # make a grid of all zeros
+    a = np.zeros((n, n), dtype="int")
+    print(a)
+    print(type(a))
+
+    # make the middle row as all 1s
+    middle_row = len(a) // 2
+    a[middle_row, :] = 1
+    # for i in range(n):
+        # a[middle_row, i] = 1
+
+    print(a)
+
+    # make the middle column as all 1s
+    middle_col = n // 2
+    a[:, middle_col] = 1
+    # for i in range(n):
+        # a[i, middle_col] = 1
+
+    print(a)
+
+size = int(input("Enter the size of the grid:\t"))
+make_plus(size)
